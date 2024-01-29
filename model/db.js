1aa68179933a544598563afc52f0c4e828565074
Ananthu <ananthuks9526@example.com>
2024-01-29 19:22:54 +0530

    Solving

--- model/db.js
+++ model/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 //connect to mongodb
 let db = async()=>{
     try {
-      await mongoose.connect('mongodb://ananthuks9526:EWyBaC1Vf2nDAfvy@ac-86esuuz-shard-00-00.yirplhg.mongodb.net:27017,ac-86esuuz-shard-00-01.yirplhg.mongodb.net:27017,ac-86esuuz-shard-00-02.yirplhg.mongodb.net:27017/?ssl=true&replicaSet=atlas-qt9xng-shard-0&authSource=admin&retryWrites=true&w=majority')
+      await mongoose.connect(process.env.MONGODB)
       console.log("Connected to Database");
     } catch (error) {
       console.log("Error in Connecting Database",error.message);
