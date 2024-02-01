0e4a02232b174f530d3fc358f10548d883063326
Ananthu <ananthuks9526@example.com>
2024-02-01 09:09:48 +0530

    Checking

--- controller/userProductsController.js
+++ controller/userProductsController.js
@@ -185,7 +185,7 @@ userPro.displaySearch = async (req, res) => {
       const currentPage = parseInt(req.query.page) || 1;
       const perPage = 8; // Number of products per page
 
-      const escapedText = name.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+      const escapedText = name.charAt(0).replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
       const regexPattern = `^${escapedText}`;
       const regex = new RegExp(regexPattern, 'i');
 
